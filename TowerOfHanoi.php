910dd159181aacc22a782d9cf105762ca376053b
shadarf779 <sa1121@cs.soran.edu.iq>
2023-10-12 01:04:41 +0300

    newer virstion

--- TowerOfHanoi.php
+++ TowerOfHanoi.php
@@ -1,37 +1,45 @@
 <?php 
 
 
-function tower_of_hanoi($n, &$A, &$B, &$C) {
+function tower_of_hanoi( $count,$n, &$A, &$B, &$C) {
+
+
     if ($n === 1) {
         $disk = array_pop($A);
         array_push($C, $disk);
+        echo $n . "<br>";
         echo "A Peg: " . implode(", ", $A) . "<br>";
         echo "B Peg: " . implode(", ", $B) . "<br>";
         echo "C Peg: " . implode(", ", $C) . "<br>";
-    echo "<hr>";
-
+        echo "<hr>";
         return;
     }
-    tower_of_hanoi($n - 1, $A, $C, $B);
+
+    tower_of_hanoi($count++ ,$n - 1, $A, $C, $B);
+
+
     $disk = array_pop($A);
     array_push($C, $disk);
+    echo $n . "<br>";
+
+    echo "Second one <br>";
     echo "A Peg: " . implode(", ", $A) . "<br>";
     echo "B Peg: " . implode(", ", $B) . "<br>";
     echo "C Peg: " . implode(", ", $C) . "<br>";
     echo "<hr>";
-    tower_of_hanoi($n - 1, $B, $A, $C);
+    tower_of_hanoi( $count++ ,$n - 1, $B, $A, $C);
 }
 
 // Example usage
 $A = [5, 4, 3, 2, 1];
 $B = [];
 $C = [];
-
+$count = 1;
 // Output the final state of A and C pegs
 echo "A Peg: " . implode(", ", $A) . "<br>";
 echo "C Peg: " . implode(", ", $C) . "<br>";
 $n = count($A);
-tower_of_hanoi($n, $A, $B, $C);
+tower_of_hanoi($count ,$n, $A, $B, $C);
 
 // Output the final state of A and C pegs
 echo "A Peg: " . implode(", ", $A) . "<br>";
