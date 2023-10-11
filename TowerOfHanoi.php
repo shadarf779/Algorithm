7afbf3edfce96cfd292720e62123d921ff2c23f9
shadarf779 <sa1121@cs.soran.edu.iq>
2023-10-12 01:13:20 +0300

    more understandable

--- TowerOfHanoi.php
+++ TowerOfHanoi.php
@@ -8,24 +8,28 @@ function tower_of_hanoi( $count,$n, &$A, &$B, &$C) {
         $disk = array_pop($A);
         array_push($C, $disk);
         echo $n . "<br>";
+    echo "First one <br>";
+
         echo "A Peg: " . implode(", ", $A) . "<br>";
         echo "B Peg: " . implode(", ", $B) . "<br>";
         echo "C Peg: " . implode(", ", $C) . "<br>";
         echo "<hr>";
         return;
     }
+    echo $n . "<br>";
 
     tower_of_hanoi($count++ ,$n - 1, $A, $C, $B);
 
 
     $disk = array_pop($A);
     array_push($C, $disk);
-    echo $n . "<br>";
 
     echo "Second one <br>";
     echo "A Peg: " . implode(", ", $A) . "<br>";
     echo "B Peg: " . implode(", ", $B) . "<br>";
     echo "C Peg: " . implode(", ", $C) . "<br>";
+    echo $n . "<br>";
+
     echo "<hr>";
     tower_of_hanoi( $count++ ,$n - 1, $B, $A, $C);
 }
