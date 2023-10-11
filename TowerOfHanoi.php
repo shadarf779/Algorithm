c8a6878bd1c43a8d75dd7d10051dfeb2bc79b2ed
shadarf779 <sa1121@cs.soran.edu.iq>
2023-10-11 22:35:57 +0300

    store data in the list

--- TowerOfHanoi.php
+++ TowerOfHanoi.php
@@ -1,19 +1,36 @@
 <?php 
 
 
-function tower_of_hanoi($n, $source, $auxiliary, $target) {
+function tower_of_hanoi($n, &$source, &$auxiliary, &$target) {
     if ($n === 1) {
-        echo "Move disk 1 from $source to $target<br>";
+        $disk = array_pop($source);
+        array_push($target, $disk);
+        echo "Move disk $disk from Source to Target<br>";
         return;
     }
+
     tower_of_hanoi($n - 1, $source, $target, $auxiliary);
-    echo "Move disk $n from $source to $target <br>";
+    $disk = array_pop($source);
+    array_push($target, $disk);
+    echo "Move disk $disk from Source to Target<br>";
     tower_of_hanoi($n - 1, $auxiliary, $source, $target);
 }
 
 // Example usage
-$n = 5;  // Number of disks
-tower_of_hanoi($n, 'A', 'B', 'C');
+$source = [5, 4, 3, 2, 1];
+$auxiliary = [];
+$target = [];
+
+// Output the final state of source and target pegs
+echo "Source Peg: " . implode(", ", $source) . "<br>";
+echo "Target Peg: " . implode(", ", $target) . "<br>";
+$n = count($source);
+tower_of_hanoi($n, $source, $auxiliary, $target);
+
+// Output the final state of source and target pegs
+echo "Source Peg: " . implode(", ", $source) . "<br>";
+echo "Target Peg: " . implode(", ", $target) . "<br>";
+
 
 
 
