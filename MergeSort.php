ea4094a8db520f732fb2f20af2ad50bc8673a57b
shadarf779 <sa1121@cs.soran.edu.iq>
2023-10-20 16:37:28 +0300

    update Merge Sort

--- MergeSort.php
+++ MergeSort.php
@@ -1,3 +1,16 @@
+<?php 
+function mergeSort($arr,$end,$start)
+{
+    $med = ($end + $start)/2;
+    $arr1 = [];
+    $arr2 = [];
+    if($){
+
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+        $arr = [34,52,80,75,7,4,2,9,12,14,3,1,16,10,0,54,8];
+        
+        mergeSort($arr,Count($arr),0);
+    ?>
 </body>
 </html>
