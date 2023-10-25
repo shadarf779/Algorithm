cd5c935e4ba5dcf68cd1caa6f9d281d9cac0d9c5
shadarf779 <sa1121@cs.soran.edu.iq>
2023-10-26 01:00:03 +0300

    Merge Sort with Explanation

--- MergeSort.php
+++ MergeSort.php
@@ -16,53 +16,92 @@ function mergeSort($arr) {
         }
     }
     
+    $left = mergeSort($left);
+    $right = mergeSort($right);
 
-    ?>
-    <table style="width: 100%;" >
+    // Recursively sort both halves
+
+    
+    // Merge the sorted halves
+    $result = merge($left, $right , $length);
+    
+    return $result;
+}
+
+function merge($left, $right, $length) {
+    $result = [];
+    $i=0;
+    $a =0;
+    $b=0;
+    ?> 
+       <table style="width: 100%;" >
         <tr >
-            <td style="width: 45%;">
+            <td style="width: 40%;">
+            <p>Left</p>
 <?php  print_r($left);?>
             </td>
-            <td style="width: 10%;">
-                
-            </td>
-            <td style="width: 45%;">
+          
+            <td style="width: 40%;">
+            <p>Right</p>
 <?php  print_r($right);?>
                 
             </td>
         </tr>
     </table>
-<?php
+    <?php
+    while($i < $length  )
+    {
+        if($a<Count($left) && $b<Count($right))
+        {
+            if($left[$a] < $right[$b])
+            {
+                $result[$i]=$left[$a];
+             echo " $left[$a] vs $right[$b] <br>";
 
-    // Recursively sort both halves
-    $left = mergeSort($left);
-    $right = mergeSort($right);
-    
-    // Merge the sorted halves
-    $result = merge($left, $right);
-    
-    return $result;
-}
+             echo  $result[$i];
+             echo " , Left Smaller <br>";
+                $a++;
+                
+            }else
+            {
+                $result[$i]=$right[$b];
+             echo " $left[$a] vs $right[$b] <br>";
 
-function merge($left, $right) {
-    $result = [];
-    
-    while (count($left) > 0 && count($right) > 0) {
-        if ($left[0] <= $right[0]) {
-            $result[] = array_shift($left);
-        } else {
-            $result[] = array_shift($right);
+                echo  $result[$i];
+                echo " , Right Smaller <br>";
+                $b++;
+            }
+        }else if($a == Count($left) && $b< count($right) )
+        {
+            $result[$i] = $right[$b];
+            
+            echo  $result[$i];
+            echo " , Left is EMTY <br>";
+            $b++;
+        }else if ($b == Count($right) && $a< count($left))
+        {
+            $result[$i] = $left[$a];
+            echo  $result[$i];
+            echo " , Right Is MT <br>";
+            $a++;
         }
+        $i++;
     }
     
-    while (count($left) > 0) {
-        $result[] = array_shift($left);
-    }
-    
-    while (count($right) > 0) {
-        $result[] = array_shift($right);
-    }
-    
+    ?>
+ 
+    <table style="width: 80%;" >
+        <tr >
+           
+          
+            <td style="width: 40%;">
+            <p>ALL</p>
+<?php  print_r($result);?>
+                
+            </td>
+        </tr>
+    </table>
+<?php
     return $result;
 }
 
@@ -71,7 +110,6 @@ $arr = [34,52,80,75,7,4,2,9,12,14,3,1,16,10,0,54,8];
 print_r($arr);
 echo "<br>";
 $sortedArray = mergeSort($arr);
-print_r($sortedArray);
 
 
 
