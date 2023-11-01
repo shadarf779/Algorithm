252b411f51b44ee88b74e1e0a6fdd40ef46ac9e3
shadarf779 <sa1121@cs.soran.edu.iq>
2023-11-01 23:11:42 +0300

    FAST MODULER

--- FastPowerExpotational.php
+++ FastPowerExpotational.php
@@ -1,36 +1,31 @@
 <?php 
 
-function fastPower($base, $exponent) {
-    
+function fastPower($base, $exponent) {    
     if ($exponent == 0) {
         return 1;
     } elseif ($exponent % 2 == 0) {
         $half_pow = fastPower($base, $exponent / 2);
-        echo "$half_pow * ";
+        echo "($half_pow * $half_pow )<br>";
         return $half_pow * $half_pow;
-    } else {
-        
+    } else {        
         $half_pow = fastPower($base, ($exponent - 1) / 2);
-        echo "$base * $half_pow * ";
+        echo "($base * $half_pow * $half_pow  )<br>";
         return $base * $half_pow * $half_pow;
     }
 }
-
 function NormalPower($base, $exponent) {
     $Pow =$base;
     $x=1;
     while ($exponent > $x){
         $Pow = $Pow *$base;
         $x++;
-        echo "$base * ";
-        
+        echo "$base * ";        
     }
     return $Pow;
 }
-
 // Example usage:
 $base = 2;
-$exponent =32;
+$exponent =31;
 $result = fastPower($base, $exponent);
 echo " = $result <br>";
 
--- Moduler.php
+++ Moduler.php
@@ -1,50 +1,34 @@
 <?php
-    function fast_moduler($x , $n , $m){
-        if($n==0){
-            return 1;
-        }else if ($n%2==0){
-            $result= fast_moduler($x , $n/2 , $m);
-            return ($result * $result)%$m;
+function fast_moduler($x, $n, $m) {
+    if ($n == 0) {
+        return 1;
+    } elseif ($n % 2 == 0) {
+        $result = fast_moduler($x, $n / 2, $m);
+        echo ("$result * $result % $m<br>");
+        echo (( $result * $result)%$m."<hr>");
+        return ($result * $result) % $m;
+    } else {
+        $result = fast_moduler($x, $n - 1, $m);
+        echo ("$x * $result * $result % $m <br>");
+        echo (($x * $result * $result)%$m."<hr>");
 
-        }else{
-            return ((($x%$m)*fast_moduler($x , $n-1,$m)%$m));
-        }
-    }
-    
-    function Binary_search($a, $left , $right , $x){
-        $m = (int) ($right + $left)/2;
-        if($left>$right){
-            return 0;
-        }else{
-        if($x==$a[$m]) {
-            return $m;
-        }
-        if($x >$a[$m]){
-            return Binary_search($a, $m+1,$right,$x);
-        }else{
-            return Binary_search($a,$left,$m-1,$x);
-        }
+        return ($x * $result* $result)%$m;
     }
 }
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Modular Exponentiation</title>
 </head>
 <body>
-    <?php 
-        $a = fast_moduler(3,100,15);
-        echo "result is $a";
-        $arr = [1,3,5,6,7,9,10,11,12,14,15,17,18,21,22,23,24,27,28];
-        $x=9;
-        echo "<br>";
-        $b = Binary_search($arr,0, Count($arr),$x);
-        echo "the Index of Numbner $x id loacted in $b";
+    <?php
+    echo ((3** 13)%5);
+    $a = fast_moduler(3, 13, 5);
+    echo "Result is $a";
     ?>
 </body>
 </html>
