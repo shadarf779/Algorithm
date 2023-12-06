86a3c30b9a65443bea27187be17790642f82929e
shadarf779 <sa1121@cs.soran.edu.iq>
2023-12-06 19:35:06 +0300

    last update

--- SchedulingProblem.php
+++ SchedulingProblem.php
@@ -1,44 +1,44 @@
 <?php
-function shortest_job($jobs , $process)
+
+function shortest_job($jobs, $process)
 {
-    $short_job=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    for ($i=0;$i<count($jobs);$i++)
-    {   
-        $index_small = min_index();
-        
+    $short_job = array_fill(0, $process, 0);
+
+    for ($i = 0; $i < count($jobs); $i++) {
+        $index_small = min_index($short_job);
         $short_job[$index_small] += $jobs[$i];
     }
-    for($i=0;$i<count($short_job);$i++)
-    {
 
-    }
+    print_r($short_job);
 }
 
-function Max_value($jobs )
+function max_value($jobs)
 {
-    $max = 0 ;
-    for($i=0; $i<count($jobs);$i++)
-    {
-        if($jobs[$i]<$max)
-        {
-            $max=$i;
+    $max = 0;
+
+    for ($i = 0; $i < count($jobs); $i++) {
+        if ($jobs[$i] > $max) {
+            $max = $jobs[$i];
         }
     }
 
+    return $max;
 }
-function min_index($jobs )
+
+function min_index($short_job)
 {
-    $min = 0 ;
-    for($i=0; $i<count($jobs);$i++)
-    {
-        if($jobs[$i]<$jobs[$min])
-        {
-            $min=$i;
+    $min = 0;
+
+    for ($i = 0; $i < count($short_job); $i++) {
+        if ($short_job[$i] < $short_job[$min]) {
+            $min = $i;
         }
     }
+
+    return $min;
 }
 
-
-$jobs = [3,5,6,10,11,14,15,18,20];
+$jobs = [3, 5, 6, 10, 11, 14, 15, 18, 20];
 $process = 3;
-shortest_job($jobs , $process);
+
+shortest_job($jobs, $process);
